c61e567dff941945885882d59821464c0aee011e
Andy <qingjielu990916@gmail.com>
2020-02-10 22:54:30 -0500

    update server logic

--- server/index.php
+++ server/index.php
@@ -1,15 +1,46 @@
 <?php
 require("./config.php");
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
-// phpinfo();
-//print_r( get_defined_functions());
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+//echo 'right';
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $res=array(
+            'status' => 0
+      );
+    echo json_encode($res);
+}else{
+  $input = $_GET["path"];
+  // query database here
+   // 0: database connection connect_error
+   //    1: success with data
+   //    2: page not found
+   //    3: wrong entry in the database
+   $sql = "SELECT configuration FROM data where path=" .$input;
+   $result = $conn->query($sql);
+
+   echo $result;
+  // if ($result->num_rows == 0) {
+  //   $res=array(
+  //           'status' => 2
+  //     );
+  //   echo json_encode($res);
+  // } else if($result->num_rows == 1){
+  //   $res=array(
+  //           'status' => 1,
+  //           'payload' => $result
+  //     );
+  //   echo json_encode($res);
+  // }else{
+  //   $res=array(
+  //           'status' => 3
+  //     );
+  //   echo json_encode($res);
+  // }
+
+$conn->close();
+
 }
-echo "Connected successfully";
+
+
+
 ?>
